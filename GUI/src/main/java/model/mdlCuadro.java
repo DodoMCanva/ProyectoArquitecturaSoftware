7053a4bc9eec2a38ed3b4f5c6c31baef46feb328
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-03-11 20:35:14 -0600

    f

---
+++ GUI/src/main/java/model/mdlCuadro.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ *
+ * @author Fer
+ */
+public class mdlCuadro {
+    
+}
---
+++ GUI/src/main/java/model/mdlPunto.java
@@ -0,0 +1,18 @@
+
+package model;
+
+/**
+ *
+ * @author Fer
+ */
+public class mdlPunto {
+    private mdlPunto arriba;
+    private mdlPunto abajo;
+    private mdlPunto izq;
+    private mdlPunto der;
+    private float x;
+    private float y;
+    
+    
+    
+}
