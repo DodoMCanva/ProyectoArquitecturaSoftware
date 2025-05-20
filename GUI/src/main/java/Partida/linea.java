20e7f032683e4ea0fac91c9f0ae1f2692e36db87
superQ78 <167489927+superQ78@users.noreply.github.com>
2025-05-20 12:59:34 -0700

    corre

--- GUI/src/main/java/Partida/linea.java
+++ GUI/src/main/java/Partida/linea.java
@@ -1,6 +1,7 @@
 package Partida;
 
 import Objetos.Jugador;
+import java.awt.Color;
 
 /**
  *
@@ -11,11 +12,13 @@ public class linea {
     private punto punto_1;
     private punto punto_2;
     private Jugador jgdr;
+    private Color color;
 
-    public linea(punto punto_1, punto punto_2, Jugador jgdr) {
+    public linea(punto punto_1, punto punto_2, Jugador jgdr, Color color) {
         this.punto_1 = punto_1;
         this.punto_2 = punto_2;
         this.jgdr = jgdr;
+        this.color = color;
     }
 
     public punto getPunto_1() {
@@ -40,6 +43,14 @@ public class linea {
 
     public void setJgdr(Jugador jgdr) {
         this.jgdr = jgdr;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
 }
